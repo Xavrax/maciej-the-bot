c913109f96d2d45c49869d867c0fc8feef14193b
Mateusz <1i13i.dahlke@gmail.com>
2020-03-27 02:13:59 +0100

    voice example test

--- src/voice/model/voice_manager.rs
+++ src/voice/model/voice_manager.rs
@@ -1,9 +1,10 @@
-use serenity::prelude::TypeMapKey;
-use parking_lot::Mutex;
-use std::sync::Arc;
-
-pub struct VoiceManager;
-
-impl TypeMapKey for VoiceManager {
-    type Value = Arc<Mutex<ClientVoiceManager>>;
-}
+// use serenity::prelude::TypeMapKey;
+// use parking_lot::Mutex;
+// use std::sync::Arc;
+// use serenity::model::prelude::voice
+//
+// pub struct VoiceManager;
+//
+// impl TypeMapKey for VoiceManager {
+//     type Value = Arc<Mutex<Manager>>;
+// }
