ea8e0e543715d75196e17a6a8c9b862e31e665fc
Xavrax <1i13i.dahlke@gmail.com>
2020-04-21 01:50:59 +0200

    changed Vec to VecDeque in queue

--- src/utils/song_queue.rs
+++ src/utils/song_queue.rs
@@ -1,11 +1,12 @@
 use crate::utils::model::song_data::SongData;
 use lazy_static::*;
 use std::sync::{MutexGuard, Mutex};
+use std::collections::VecDeque;
 
 lazy_static! {
-    static ref SONG_QUEUE : Mutex<Vec<SongData>> = Mutex::new(Vec::new());
+    static ref SONG_QUEUE : Mutex<VecDeque<SongData>> = Mutex::new(VecDeque::new());
 }
 
-pub fn song_queue() -> MutexGuard<'static, Vec<SongData>> {
+pub fn song_queue() -> MutexGuard<'static, VecDeque<SongData>> {
     SONG_QUEUE.lock().unwrap()
 }
