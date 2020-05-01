819e08dbd82edadd8edfb70a5baf12bcee84a519
Xavrax <1i13i.dahlke@gmail.com>
2020-05-01 03:22:24 +0200

    add skip command

--- src/discord/commands/player.rs
+++ src/discord/commands/player.rs
@@ -10,8 +10,9 @@ use serenity::{
         macros::command
     }
 };
-use crate::utils::global::song_queue;
+use crate::utils::global::{song_queue, current_song};
 use crate::utils::model::song_data::SongData;
+use crate::utils::music::play_next;
 
 #[command]
 pub fn play(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
@@ -30,7 +31,12 @@ pub fn play(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
 pub fn skip(ctx: &mut Context, msg: &Message) -> CommandResult {
     check_msg(msg.channel_id.say(&ctx.http, "Skipping..."));
 
-    // current_song().audio
+    if song_queue().is_empty() {
+        current_song().audio.lock().pause();
+        current_song().audio.lock().finished = true;
+    } else {
+        play_next();
+    }
 
     Ok(())
 }
