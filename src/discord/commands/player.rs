4d891c2031550f3feb426d0149e72b11a9cb7a00
Xavrax <1i13i.dahlke@gmail.com>
2020-05-01 03:01:20 +0200

    delete unused code from player

--- src/discord/commands/player.rs
+++ src/discord/commands/player.rs
@@ -18,14 +18,6 @@ use crate::utils::model::song_data::SongData;
 #[command]
 pub fn play(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
     let impl_song = args.message();
-    // let impl_song = match args.single::<String>() {
-    //     Ok(impl_song) => impl_song,
-    //     Err(_) => {
-    //         check_msg(msg.channel_id.say(&ctx.http, "Must provide a URL to a video or audio"));
-    //
-    //         return Ok(());
-    //     },
-    // };
 
     song_queue().push_back(
         SongData::new(impl_song.to_string(), ctx.clone(), msg.clone())
