2ffc3b26e0aacf856813c8ce8d64322789e91f87
Xavrax <1i13i.dahlke@gmail.com>
2021-09-30 18:49:01 +0000

    cargo fix

--- src/bin/maciej-the-bot.rs
+++ src/bin/maciej-the-bot.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use maciej_the_bot::help_message;
-use std::str::from_utf8;
-use tokio::fs::File;
-use tokio::io::AsyncReadExt;
+
+
+
 
 #[tokio::main]
 async fn main() -> Result<()> {
