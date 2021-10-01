f8dac2812d6d3a48092dd39a5bb17dd7296198a5
Xavrax <1i13i.dahlke@gmail.com>
2021-10-01 23:00:50 +0000

    add ut for prefix

--- src/command/help.rs
+++ src/command/help.rs
@@ -38,6 +38,18 @@ mod should {
         then_should_reply_with_help(&mut env);
     }
 
+    #[tokio::test]
+    async fn replace_placeholders_to_prefix() {
+        let mut env = ScenarioEnvironment::default();
+
+        given_prefix(&mut env);
+        and_given_discord_facade_expecting_prefix(&mut env);
+        when_command_is_triggered(&mut env).await;
+        then_prefix_should_be_included_to_message(&mut env);
+    }
+
+
+
     mod dsl {
         use crate::discord_facade::MockDiscordFacade;
         use anyhow::Result;
@@ -47,6 +59,7 @@ mod should {
         #[derive(Default)]
         pub struct ScenarioEnvironment {
             pub facade: Option<MockDiscordFacade>,
+            pub prefix: Option<String>,
 
             pub result: Option<Result<()>>,
         }
@@ -60,6 +73,21 @@ mod should {
             })
         }
 
+        pub fn and_given_discord_facade_expecting_prefix(env: &mut ScenarioEnvironment) {
+            env.facade = Some({
+                let prefix = env.prefix.take().unwrap();
+
+                let mut mock = MockDiscordFacade::new();
+                mock.expect_reply().withf(move |content| content.contains(&prefix) ).times(1).return_once(|_| Ok(()));
+
+                mock
+            })
+        }
+
+        pub fn given_prefix(env: &mut ScenarioEnvironment) {
+            env.prefix = Some("!".into());
+        }
+
         pub async fn when_command_is_triggered(env: &mut ScenarioEnvironment) {
             env.result = Some(HelpCommand.execute(env.facade.take().unwrap()).await);
         }
@@ -68,5 +96,8 @@ mod should {
             assert!(env.result.take().unwrap().is_ok());
         }
 
+        pub fn then_prefix_should_be_included_to_message(env: &mut ScenarioEnvironment) {
+            assert!(env.result.take().unwrap().is_ok());
+        }
     }
 }
